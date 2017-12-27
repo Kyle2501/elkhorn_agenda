7c8d6ddf438efa2968981bb5df749d6d5d40fbd6
kyle2501 <kyle2501@gmail.com>
2017-12-26 17:25:49 -1000

    . added week view code

--- webapp_folder/agenda-server.py
+++ webapp_folder/agenda-server.py
@@ -12,6 +12,8 @@ import cgi
 import urllib
 import wsgiref.handlers
 import datetime
+import time
+import calendar as cal
 import json, ast
 import sys,imp
   # - Appengine
@@ -25,11 +27,67 @@ import webapp2
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext.webapp.util import run_wsgi_app
-
+from pytz.gae import pytz
 
 import _html as _html
 
 
+
+
+#----------------------------------------------#
+#        Completed Data Stucture               #
+#----------------------------------------------#
+class Agenda_db(ndb.Model):
+    data_id = ndb.StringProperty()
+#
+    user_id = ndb.StringProperty()
+    user_email = ndb.StringProperty()
+#
+    item_id = ndb.StringProperty()
+    item_name = ndb.StringProperty()
+    item_kind = ndb.StringProperty()
+#
+    item_status = ndb.StringProperty()
+    status_date = ndb.StringProperty()
+
+    @classmethod
+    def _get_my_status(self):
+      client_email = users.get_current_user().email()
+      q = People_db.query(People_db.user_email == client_email)
+      db_data = []
+      for item in q.iter():
+        db_data.append(item.to_dict(exclude=['user_id','user_email']))
+      return json.dumps(db_data)
+
+
+class updatePeople_db(webapp2.RequestHandler):
+  def post(self):
+    page_address = self.request.uri
+    base = os.path.basename(page_address)
+    
+    user = users.get_current_user()
+    if user:
+        item_id = self.request.get('item_id')
+        client_email = user.email()
+        key_name = item_id + '_' + client_email
+        item = People_db.get_by_id(key_name)
+        
+        if not item:
+            item = People_db(id=key_name)
+        
+        item.user_id = user.user_id()
+        item.user_email = user.email()
+        item.status_date = datetime.datetime.now(pytz.timezone(Timezone)).strftime("%Y/%m/%d %H:%M:%S")
+        
+
+        item.item_id = self.request.get('item_id')
+        item.item_name = self.request.get('item_name')
+        item.item_kind = self.request.get('item_kind')
+        item.item_status = self.request.get('item_status')
+
+        item.put()
+        
+    self.redirect('/my_info')
 
 
 
@@ -187,6 +245,60 @@ class publicSite(webapp2.RequestHandler):
             page_name = 'My Info'
             nav_select = 'my_info'
             user_header = 'on'
+            
+        today_date = datetime.datetime.now(pytz.timezone(Timezone))
+        weekday = datetime.date.isoweekday(today_date)
+        week_number = today_date.strftime("%W")
+      #
+        month_number = today_date.strftime("%m")
+        month_name = today_date.strftime("%B")
+        month_day = today_date.strftime("%d")
+        year_day = today_date.strftime("%j")
+        year = today_date.strftime("%Y")
+
+
+        day_of_the_month = int(month_day)
+        day_of_week = int(weekday)
+        days_left = 7 - day_of_week
+        monday_number = day_of_the_month - day_of_week + 1
+        sunday_number = day_of_the_month + days_left
+
+        day_number = [1,2,3,4,5,6,7]
+        day_name = ['Mon','Tues','Wed','Thurs','Fri','Satur','Sun']
+        week_range = []
+        for _ in range(int(monday_number), int(sunday_number)):
+            week_range.append(_)
+
+
+        week_day_keys = ['day_name','day_number','month_day']
+
+        for value in range(4):
+            test = {key: value for key in week_day_keys}
+
+
+        calendar = cal.monthrange(int(year),int(month_number))
+        future_months_1 = 0 # cal.monthrange(int(year),int(int(month_number) + 1))
+        future_months_2 = 0 # cal.monthrange(int(year),int(int(month_number) + 2))
+        future_months_3 = 0 # cal.monthrange(int(year),int(int(month_number) + 5))
+        future_months_4 = 0 # cal.monthrange(int(year),int(int(month_number) + 5))
+        future_months_5 = 0 # cal.monthrange(int(year),int(int(month_number) + 5))
+        calendar2 = calendar
+        days_month = calendar[1]
+        calendar = days_month
+        days_in_month = int(days_month)
+
+        if sunday_number <= days_in_month:
+            sunday = ''
+            
+        week_days = [
+          {'day_number': 1,'day_name': 'Mon','month_day': ''},
+          {'day_number': 2,'day_name': 'Tues','month_day': ''},
+          {'day_number': 3,'day_name': 'Wed','month_day': ''},
+          {'day_number': 4,'day_name': 'Thurs','month_day': ''},
+          {'day_number': 5,'day_name': 'Fri','month_day': ''},
+          {'day_number': 6,'day_name': 'Satur','month_day': ''},
+          {'day_number': 7,'day_name': 'Sun','month_day': ''}
+        ]
 
 
       # - template
@@ -201,6 +313,22 @@ class publicSite(webapp2.RequestHandler):
             'nav_select': nav_select,
         
             'page_html': page_html,
+
+
+                    'today_date': today_date,
+        'weekday': weekday,
+        'week_number': week_number,
+        'month_name': month_name,
+        'month_day': month_day,
+        'year_day': year_day,
+        'year': year,
+
+        'calendar': calendar,
+        'days_left': days_left,
+        'week_range': week_range,
+        'week_days': week_days,
+        'day_of_week': day_of_week,
+        'days_month': days_month,
         
         
         
